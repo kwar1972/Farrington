84114aaec9ab54cc8eae35fcfdab822577aa1d50
kwar1972 <aeghysels@gmail.com>
2019-09-25 08:30:05 -0300

    Myholdings totear indicator and number format

--- public/assets/js/myholdings.js
+++ public/assets/js/myholdings.js
@@ -38,7 +38,26 @@ function settableHoldings() {
               { "data": "pricesell", render: $.fn.dataTable.render.number( ',', '.', 2, '$ ' )  },
               { "data": "totalpaid", render: $.fn.dataTable.render.number( ',', '.', 2, '$ ' )  },
               { "data": "totpos", render: $.fn.dataTable.render.number( ',', '.', 2, '$ ' )  },
-              { "data": "totearn", render: $.fn.dataTable.render.number( ',', '.', 2, '$ ' )  },
+              { mRender: function (data, type, row) {
+                if(row.totearn > 0){
+                  function currencyFormat(num) {
+                    return '$ ' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+                  }
+                  var number = row.totearn;
+                  var number2 = currencyFormat(number);
+                  var up = '<span class="g-color-green"><i class="fas fa-sort-amount-up"></i></span> '+ number2
+                  return up
+                }else{
+                  function currencyFormat(number) {
+                    return '$ ' + number.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+                  }
+                  var number = row.totearn;
+                  var number2 = currencyFormat(number);
+                  var down = '<span class="g-color-red"><i class="fas fa-sort-amount-down"></i></span> '+ number2
+                  return down
+                }
+              }
+               },
               { "data": "totsold", render: $.fn.dataTable.render.number( ',', '.', 2, '$ ' )  },
             ]
       });
