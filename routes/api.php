2f319b2044e0e7908cc04decf9538c24a5bb554a
kwar1972 <aeghysels@gmail.com>
2019-09-27 13:26:47 -0300

    Small fix

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/debug/{$state}', function ($state) {
+    dd($state);
+    Artisan::call($state);
+    
+    return redirect('home');
+});
