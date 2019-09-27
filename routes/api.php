ada50a50053c16577cf993f3ea119c7a05d4283d
kwar1972 <aeghysels@gmail.com>
2019-09-27 13:44:56 -0300

    Small fix

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/debug/{$state}', function ($state) {
-    dd($state);
+   
     Artisan::call($state);
     
     return redirect('home');
