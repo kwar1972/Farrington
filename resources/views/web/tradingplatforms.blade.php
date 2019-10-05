b57d1dd68db4e5e43c2c558eecfdef6110c261f1
kwar1972 <aeghysels@gmail.com>
2019-10-05 18:47:52 -0300

    Account Types 2

--- resources/views/web/tradingplatforms.blade.php
+++ resources/views/web/tradingplatforms.blade.php
@@ -1,7 +1,63 @@
 @extends('layouts.web.app') 
 @section('content')
-
-
-
-
+<div class="container-fluid g-pt-120">
+        <header class="text-center g-width-60x--md mx-auto g-mb-60">
+                <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+                  <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Trading <span class="g-color-primary">Platforms</span></h2>
+                </div>
+                <p class="lead text-center"><span class="g-color-primary">Steiner Greves</span> have source the best selection of Online trading platforms to give you more freedom at the touch of a button.</p>
+              </header>
+    <div class="card-group g-mx-10 g-my-30">
+        <div class="card g-mx-10 u-shadow-v1-2">
+            <img class="card-img-top" src="img/baxter-and-grant-austria-trading-company.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Live Integrated Reports</h4>
+                <p class="card-text">Check out the latest economic news, updates and important events of the global market around the world. With full antivirus protection and secure AES-256 encryption. </p>
+            </div>
+        </div>
+        <div class="card g-mx-10 u-shadow-v1-2">
+            <img class="card-img-top" src="img/baxter-and-grant-austria-stock-market-company.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Powerful Trading Charts</h4>
+                <p class="card-text">Be one step ahead of the market’s next move! Analyze trends and grab opportunities as they occur with the help of over 60 indicators, interactive chart tools and timeframes.</p>
+            </div>
+        </div>
+        <div class="card g-mx-10 u-shadow-v1-2">
+            <img class="card-img-top" src="img/baxter-and-grant-investment-company.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Guaranteed Pricing</h4>
+                <p class="card-text">NO requotes, NO slippage! Plus,
+                    Direct access to a wide range of global assets, including forex currencies, metals, commodities, stocks and indices</p>
+            </div>
+        </div>
+        <div class="card g-mx-10 u-shadow-v1-2">
+            <img class="card-img-top" src="img/baxter-and-grant-investment-online-trading.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Secure Trading on the Go</h4>
+                <p class="card-text">Forex and Derivative Trading with the highest security standards! MetaTrader4 allows you to trade instantly from any web browser, Android or iPhone/iPad device.</p>
+            </div>
+        </div>
+    </div>
+    <header class="text-center g-width-60x--md mx-auto g-mb-60">
+            <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+              <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Robust Trading <span class="g-color-primary">Execution</span></h2>
+            </div>
+            <p class="lead text-center"><span class="g-color-primary">Synapsis v2.0</span> , a platform you can depend on!</p>
+          </header>
+    
+</div>
+<!-- Call To Action -->
+<section class="g-bg-primary g-color-white w-100 g-pa-30" style="background-image: url(../../assets/img/bg/pattern5.png);">
+    <div class="justify-content-md-center text-center">
+      <div class="align-self-md-center">
+        <p class="lead g-font-weight-400 g-mr-20--md g-mb-15 g-mb-0--md g-color-black g-font-weight-600">NOT A CLIENT? OPEN A TRADING ACCOUNT TODAY</p>
+        <p class="g-color-black">Take advantage of our competitive trading Strategies</p>
+      </div>
+      <div class="align-self-md-center">
+        <a class="btn btn-lg u-btn-black text-uppercase g-font-weight-600 g-font-size-12" target="_blank" href="#">Register</a>
+        <a class="btn btn-lg u-btn-white text-uppercase g-font-weight-600 g-font-size-12" target="_blank" href="#">Account Types</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Call To Action -->
 @endsection
