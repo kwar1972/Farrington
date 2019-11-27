e9d763b51f43f81b11babd7f5668b0b3ac73e430
kwar1972 <aeghysels@gmail.com>
2019-11-27 08:06:32 -0300

    Added new section investment process

---
+++ resources/views/web/investpro.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.web.app') 
+@section('content')
+<div class="container-fluid g-pt-120">
+        <header class="text-center g-width-60x--md mx-auto g-mb-60">
+                <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+                  <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600">Our <span class="g-color-primary"> Investment Process</span></h2>
+                </div>
+                <p class="lead text-center">Benefits and trading conditions ideal for individual traders.</p>
+              </header>
+
+    <div class="row">
+        <div class="col-sm-12">
+          <!-- Dynamic Process -->
+          <div id="carousel3" class="js-carousel" data-infinite="true" data-pagi-classes="js-pagination u-carousel-indicators-v30 g-pos-abs g-top-0">
+            <div class="js-slide g-pt-70 g-pt-150--sm" data-title="Step 1">
+            <!-- Dynamic Process -->
+            <div class="row no-gutters">
+              <div class="col-lg-6 g-bg-black g-pa-60 g-pa-100--sm">
+                <h2 class="h4 g-color-primary g-font-weight-700 g-mb-25">Having an understanding of your needs</h2>
+                <h3 class="h4 g-color-white g-font-weight-700 g-mb-25">Your investor profile</h3>
+              
+                <div class="g-mb-50">
+                  <p class="g-color-white-opacity-0_7 g-mb-30">Your personal Finance Manager will spend an appreciable amount of time reviewing your personal
+                    circumstances, your financial needs and goals and your attitude to different levels of risk. This in depth
+                    understanding is beneficial in developing a wealth management strategy that's custom-built to your
+                    needs.</p>
+                  <p class="g-color-white-opacity-0_7 g-mb-30">Among the things you will discuss in your initial consultation, which can be face to face or over the
+                    phone, will include:</p>
+                    <ul>
+                      <li class="g-color-primary">Your investment aims and objectives</li>
+                      <li class="g-color-primary">The level of risk you feel comfortable with</li>
+                      <li class="g-color-primary">Your knowledge and experience of investing</li>
+                      <li class="g-color-primary">Current investments you may have</li>
+                      <li class="g-color-primary">The capital you have to invest</li>
+                      <li class="g-color-primary">Your time horizon</li>
+                    </ul>
+                </div>
+              
+                <a class="btn btn-lg u-btn-primary g-color-black g-font-weight-600 g-font-size-default text-uppercase g-rounded-50" href="/contact">Contact us for more info</a>
+              </div>
+            
+              <div class="col-lg-6 g-min-height-600 g-bg-size-cover g-bg-pos-center" style="background-image: url(../images/needs.jpg);"></div>
+            </div>
+            <!-- End Dynamic Process -->
+          </div>
+                   <div class="js-slide g-pt-70 g-pt-150--sm" data-title="Step 2">
+            <!-- Dynamic Process -->
+            <div class="row no-gutters">
+              <div class="col-lg-6 g-bg-black g-pa-60 g-pa-100--sm">
+                <h2 class="h4 g-color-primary g-font-weight-700 g-mb-25">Devising your personal strategy</h2>
+                <h3 class="h4 g-color-white g-font-weight-700 g-mb-25">A balanced asset allocation</h3>
+              
+                <div class="g-mb-50">
+                  <p class="g-color-white-opacity-0_7 g-mb-30">Following your introductory consultation, your Finance Manager will then set about determining the
+                      types of product that fit your profile. To help you achieve a balanced asset allocation in line with your
+                      investment strategy, they will match you with one of our risk-adjusted model portfolios, especially
+                      designed for internationally-minded Steiner Greves clients. These model portfolios are designed to
+                      address your investment objectives, risk tolerance and time-frame.</p>
+                  <p class="g-color-white-opacity-0_7 g-mb-30">Your Finance Manager will aim to diversify your investments across a number of diverse asset classes,
+                      to design a portfolio with a lower risk level than that associated with some standalone individual assets.
+                      This way, your potential for return is optimized for given level of risk you take.</p>
+                </div>
+              
+                <a class="btn btn-lg u-btn-primary g-color-black g-font-weight-600 g-font-size-default text-uppercase g-rounded-50" href="/contact">Contact us for more info</a>
+              </div>
+            
+              <div class="col-lg-6 g-min-height-600 g-bg-size-cover g-bg-pos-center" style="background-image: url(../images/strategy.png);"></div>
+            </div>
+            <!-- End Dynamic Process -->
+          </div>
+                   <div class="js-slide g-pt-70 g-pt-150--sm" data-title="Step 3">
+            <!-- Dynamic Process -->
+            <div class="row no-gutters">
+              <div class="col-lg-6 g-bg-black g-pa-60 g-pa-100--sm">
+                <h2 class="h4 g-color-primary g-font-weight-700 g-mb-25">Finding the Right Products for you</h2>
+                <h3 class="h4 g-color-white g-font-weight-700 g-mb-25">Selecting a range of investment products</h3>
+              
+                <div class="g-mb-50">
+                  <p class="g-color-white-opacity-0_7 g-mb-30">As soon as your wealth management approach has been agreed upon and the optimum asset allocation
+                      has been determined, your Finance Manager will recommend a choice of investment products to make
+                      the most of your assets and to help achieve the goals you've set.
+                      Our investment range includes:</p>
+                  <ul>
+                    <li class="g-color-primary">Over 100 Investment Funds</li>
+                    <li class="g-color-primary">Structured Products</li>
+                    <li class="g-color-primary">Foreign Exchange solutions</li>
+                    <li class="g-color-primary">Insurance Products</li>
+                    <li class="g-color-primary">Investment ISAs</li>
+                    <li class="g-color-primary">Brokerage products</li>
+                  </ul>
+                </div>
+              
+                <a class="btn btn-lg u-btn-primary g-color-black g-font-weight-600 g-font-size-default text-uppercase g-rounded-50" href="/contact">Contact us for more info</a>
+              </div>
+            
+              <div class="col-lg-6 g-min-height-600 g-bg-size-cover g-bg-pos-center" style="background-image: url(../images/invprod.jpg);"></div>
+            </div>
+            <!-- End Dynamic Process -->
+          </div>
+                   <div class="js-slide g-pt-70 g-pt-150--sm" data-title="Step 4">
+            <!-- Dynamic Process -->
+            <div class="row no-gutters">
+              <div class="col-lg-6 g-bg-black g-pa-60 g-pa-100--sm">
+                <h2 class="h4 g-color-primary g-font-weight-700 g-mb-25">Ongoing Review and advice</h2>
+                <div class="g-mb-50">
+                  <p class="g-color-white-opacity-0_7 g-mb-30">It is inevitable that your personal circumstances and market conditions will change, so it is very
+                      essential to review your portfolio with your Finance Manager to make certain that your strategy keeps
+                      in line with any changes and your new financial goals. A review can be requested at any time.</p>
+                </div>
+              
+                <a class="btn btn-lg u-btn-primary g-color-black g-font-weight-600 g-font-size-default text-uppercase g-rounded-50" href="/contact">Contact us for more info</a>
+              </div>
+            
+              <div class="col-lg-6 g-min-height-600 g-bg-size-cover g-bg-pos-center" style="background-image: url(../images/advice.jpg);"></div>
+            </div>
+            <!-- End Dynamic Process -->
+          </div>
+      </div>
+    </div>
+</div>
+
+@include('partials.calltoaction') 
+@endsection
