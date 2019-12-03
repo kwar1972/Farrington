e5bc8f9bf1bf0fd8a7d9d7ef6699729516dff875
kwar1972 <aeghysels@gmail.com>
2019-12-03 15:51:27 -0300

    Equities Updated

--- resources/views/web/equitystock.blade.php
+++ resources/views/web/equitystock.blade.php
@@ -10,23 +10,38 @@
         <div class="col-sm-7 g-px-120">
             <div class="row">
                 <div class="col-sm-6">
-                    <img src="img/equity-1.jpg" alt="">
+                    <img class="img-fluid" src="img/equities1.jpg" alt="" >
                 </div>
                 <div class="col-sm-6">
-                    <img src="img/equity-2.jpg" alt="">
+                    <img class="img-fluid pl-5" src="img/equities2.jpg" alt="" >
                 </div>
             </div>
-            <p class="text-justify g-pr-40 pt-4"><strong>We have a sound understanding of the financial equity markets and the importance that they serve to our clients’ success. This is why when we present a recommendation to you, it has already been thoroughly researched and analytically proven to be worthy for your consideration as a viable investment opportunity.</strong></p>
-            <p class="text-justify g-pr-40 pt-2">We do not present clients with recommendations that are based on hearsay or historical performance, far from it. The disciplined approach adopted by our award-winning team of analysts, who constantly research markets for opportunities that are not only current, but worthy of meeting the exacting demands that our clients expect. This disciplined approach to market analysis is why, on occasions, we will offer opportunities that our competitors may deem ‘out-of-the box’.</p>
+            <p class="text-justify g-pr-40 pt-4"><strong>We have a unique understanding of the equity markets and the significance it has for our
+              clients success. When we present a new recommendation to our clients, we have fully
+              researched and analytically proved the recommendation to be reliable for their
+              consideration, as a viable investment opportunity.</strong></p>
+            <p class="text-justify g-pr-40 pt-2">We do not present clients with recommendations that are based on hearsay or historical
+              performance, far from it. The structured approach adopted by our team of analysts, of
+              which are continually researching the markets for opportunities that are not only current,
+              but worthy of meeting the exacting demands that our clients expect. This disciplined
+              approach to market analysis is why, on occasions, we will offer opportunities that our
+              competitors may deem ‘out-of-the box’.</p>
 
-            <p class="text-justify g-pr-40 pt-2">As Warren Buffett famously said, “We don’t have to be smarter than the rest. We have to be more disciplined that the rest,” and at <span class="g-color-primary">Steiner Greves</span>, it is our disciplined approach that ensures we only ever present thoroughly researched opportunities to our clients, which allows our continued success to be reflected by that of theirs.</p>
+            <p class="text-justify g-pr-40 pt-2">Even if your past experience and interests is in Blue-chip Stock, Ordinary Shares, Initial
+              Public Offerings, (IPOs), Startup Funding, Preferred Stock or Common Stock. We will
+              present you with analytically researched investment opportunities, over a diverse range
+              of financial products, in numerous markets and on various platforms which are worthy for
+              your consideration, is of priority to us.</p>
                     
-            <p class="text-justify g-pr-40 pt-2">Whether your previous experience and interests related to Blue-chip Stock, Ordinary Shares, Initial Public Offerings, (IPOs), Start-up Funding, Preferred Stock or Common Stock is not important to us. Presenting you with analytically researched investment opportunities, over a diverse range of financial products, in multiple markets and on numerous platforms which are worthy for your consideration, is of importance to us.</p>
+            <p class="text-justify g-pr-40 pt-2">Our equity advisors and managers are fully qualified to impart their knowledge, in
+              conjunction with that of our analysts, with you. They thrive on being able to offer their
+              clients opportunities that they know are thoroughly researched winners.</p>
                     
-            <p class="text-justify g-pr-40 pt-2">All of our equity advisors and managers are qualified to impart their knowledge, in conjunction with that of our analysts, with you. They thrive on being able to offer their clients opportunities that they know are thoroughly researched winners.</p>
-                    
-            <p class="text-justify g-pr-40 pt-2">This ensures that we not only earn but build and maintain our reputation with you as a company that you can rely upon for its valued source of impartial and beneficial investment advice to its clients.</p>
+            <p class="text-justify g-pr-40 pt-2 pb-5">This ensures that we not only earn but build and maintain our reputation with you as a
+              company that you can rely upon for its valued source of impartial and beneficial
+              investment advice to its clients.</p>
         </div>
+        
         <div class="col-sm-5 g-pr-40">
             <div id="accordion-06" class="u-accordion" role="tablist" aria-multiselectable="true">
                   <!-- Card -->
@@ -116,6 +131,96 @@
                 <!-- End Card -->
         </div>
     </div>
+    <div class="row g-mb-60">
+        <header class="text-center g-width-60x--md mx-auto g-mb-20">
+          <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+            <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Equities <span class="g-color-primary">at a glance</span></h2>
+          </div>
+        </header>
+      <div class="col-sm-2"></div>
+      <div class="col-sm-4">
+          <img class="img-fluid float-right pl-5" src="img/equities2.jpg" alt="" >
+      </div>
+      <div class="col-sm-4">
+        <ul>
+          <li>Trade in 16 Stock exchanges around the globe.</li>
+          <li>An extensive range of financial markets within your reach.</li>
+          <li>Stretch the risk across an extensive range of locations and markets.</li>
+          <li>In equities you are likely to get much better returns, more than you would when
+           investing in cash deposits or bonds.</li>
+        </ul>
+      </div>
+      <div class="col-sm-2"></div>
+  </div>
+  <div class="row g-mb-60">
+      <header class="text-center g-width-60x--md mx-auto g-mb-20">
+        <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+          <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">What is <span class="g-color-primary">equity investment?</span></h2>
+        </div>
+      </header>
+    <div class="col-sm-2"></div>
+    <div class="col-sm-4">
+        <img class="img-fluid float-right pl-5" src="img/equities2.jpg" alt="" >
+    </div>
+    <div class="col-sm-4">
+      <p class="text-justify g-pr-40">Equity investment is when you buy one or more shares in a company and invest directly
+          in that business.</p>
+      <p class="text-justify g-pr-40">You buy your equity shares and receive returns in the form of a dividend which the
+          company may choose to pay – often annually, however it can be at the company's
+          discretion. It's important to note that the value of your equity investment may increase
+          or fall depending on the underlying value of the company.Many investors, treat equity
+          trading as a large portion of their wealth management strategy and, if you choose to
+          increase your wealth across the world, equity trading via a wealth management firm is
+          one way of doing this.</p>
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
+  <div class="row">
+      <header class="text-center g-width-60x--md mx-auto g-mb-20">
+        <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+          <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Who would benefit from <span class="g-color-primary">investing in equities?</span></h2>
+        </div>
+      </header>
+    <div class="col-sm-2"></div>
+    <div class="col-sm-4">
+        <img class="img-fluid float-right pl-5" src="img/equities2.jpg" alt="" >
+    </div>
+    <div class="col-sm-4">
+      <p class="text-justify g-pr-40">Stock markets can fluctuate this is due to country economics and market stress. For this
+          reason equities are suited for those who:</p>
+          <ul>
+            <li>Are willing to invest for the extensive term.</li>
+            <li>Have an awareness and understanding of the market accept that while they are
+                likely to receive great returns, they can also potentially lose money.</li>
+          </ul>
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
+  <div class="row g-mb-60">
+      <header class="text-center g-width-60x--md mx-auto g-mb-20">
+        <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+          <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Risks <span class="g-color-primary">and benefits</span></h2>
+        </div>
+      </header>
+    <div class="col-sm-2"></div>
+    <div class="col-sm-4">
+        <img class="img-fluid float-right pl-5" src="img/equities2.jpg" alt="" >
+    </div>
+    <div class="col-sm-4">
+          <ul>
+            <li>You will potentially get higher returns than if you invested in cash deposits bonds.</li>
+            <li>The cost of stocks and shares may rise over time, making your portfolio worth
+                probably more in the future.</li>
+            <li>The returns you will get are not known so it can be hard to plan your finances. You
+                may lose all your investment, if the company you invested in goes bankrupt.</li>
+            <li>The cost of a stock can shift in the financial market and fluctuation can be
+                extreme. Consequently you will experience investment loss if the cost falls. Past
+                performance is not indicative of future results, investments can go down as well as
+                up.</li>
+          </ul>
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
 </div>
 @include('partials.calltoaction') 
 @endsection
