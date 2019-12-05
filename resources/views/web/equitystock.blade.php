2aa16b93ed478d747018f813850af7c5450295dd
kwar1972 <aeghysels@gmail.com>
2019-12-05 10:24:52 -0300

    equity updated

--- resources/views/web/equitystock.blade.php
+++ resources/views/web/equitystock.blade.php
@@ -7,7 +7,8 @@
       </div>
     </header>
     <div class="row">
-        <div class="col-sm-7">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
             <div class="row">
                 <div class="col-sm-12 col-md-6 text-center">
                     <img class="img-fluid" src="img/equities1.jpg" alt="" >
@@ -41,140 +42,60 @@
               company that you can rely upon for its valued source of impartial and beneficial
               investment advice to its clients.</p>
         </div>
-        
-        <div class="col-sm-5 ">
-            <div id="accordion-06" class="u-accordion" role="tablist" aria-multiselectable="true">
-                  <!-- Card -->
-                  <div class="card rounded-0 g-bg-gray-light-v4 g-brd-none">
-                    <div id="accordion-06-heading-01" class="u-accordion__header g-pa-20" role="tab">
-                      <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700">
-                        <a class="d-flex justify-content-between g-color-primary g-text-underline--none--hover" href="#accordion-06-body-01" data-toggle="collapse" data-parent="#accordion-06" aria-expanded="true" aria-controls="accordion-06-body-01">
-                          Market Growth
-                          <span class="u-accordion__control-icon g-ml-10">
-                            <i class="fa fa-angle-down"></i>
-                            <i class="fa fa-angle-up"></i>
-                          </span>
-                        </a>
-                      </h5>
-                    </div>
-                    <div id="accordion-06-body-01" class="collapse show" role="tabpanel" aria-labelledby="accordion-06-heading-01" data-parent="#accordion-06">
-                      <div class="u-accordion__body g-color-gray-dark-v5 g-pa-0-20-20 text-justify">
-                             <p>A rise in interest for a particular product or service over a period of time. Market growth can be delayed if buyers do not adopt a high demand or find a product or service practical for their price level.</p>
-                             <p>For example, a current technology might only be marketable to a limited set of buyers, however if the price of the technology reduces and its benefits in every day life increase, more buyers could strengthen demand. Our expert analyst are continually watching the movement ofastock and can almost determine when a particular product will hit its bottom or hit its high.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- End Card -->
-                
-                  <!-- Card -->
-                  <div class="card rounded-0 g-bg-gray-light-v3 g-brd-none">
-                    <div id="accordion-06-heading-02" class="u-accordion__header g-pa-20" role="tab">
-                      <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700">
-                        <a class="collapsed d-flex justify-content-between g-color-primary g-text-underline--none--hover" href="#accordion-06-body-02" data-toggle="collapse" data-parent="#accordion-06" aria-expanded="false" aria-controls="accordion-06-body-02">
-                          Getting the best returns
-                          <span class="u-accordion__control-icon g-ml-10">
-                            <i class="fa fa-angle-down"></i>
-                            <i class="fa fa-angle-up"></i>
-                          </span>
-                        </a>
-                      </h5>
-                    </div>
-                    <div id="accordion-06-body-02" class="collapse" role="tabpanel" aria-labelledby="accordion-06-heading-02" data-parent="#accordion-06">
-                      <div class="u-accordion__body g-color-gray-dark-v5 g-pa-0-20-20 text-justify">
-                             <p>I am sure we all remember when we received that very first pay-check? The first time you could see the difference in what you made and what you took home. There’s a similarity when investing. There is usually a gap in the amount your portfolio earns in investment returns andinwhat actually lands in your pocket. However the difference between that and your pay-check, is the investment gap is something you can mitigate.</p>
-                             <p><span class="g-color-primary">Steiner Greves</span> can help in keeping your investor returns which can be and often are swallowed up by outrageous fees, bad investor behavior (for example terrible market timing or uncompensated risk from poor diversification), andotherinefficiencies like a bad tax approach. This is the main reason <span class="g-color-primary">Steiner Greves</span> exists to give all our clients a more secure effective and more rewarding way to invest.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- End Card -->
-                
-                  <!-- Card -->
-                  <div class="card rounded-0 g-bg-gray-light-v4 g-brd-none">
-                    <div id="accordion-06-heading-03" class="u-accordion__header g-pa-20" role="tab">
-                      <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700">
-                        <a class="collapsed d-flex justify-content-between g-color-primary g-text-underline--none--hover" href="#accordion-06-body-03" data-toggle="collapse" data-parent="#accordion-06" aria-expanded="false" aria-controls="accordion-06-body-03">
-                          Buy and build strategy
-                          <span class="u-accordion__control-icon g-ml-10">
-                            <i class="fa fa-angle-down"></i>
-                            <i class="fa fa-angle-up"></i>
-                          </span>
-                        </a>
-                      </h5>
-                    </div>
-                    <div id="accordion-06-body-03" class="collapse" role="tabpanel" aria-labelledby="accordion-06-heading-03" data-parent="#accordion-06">
-                      <div class="u-accordion__body g-color-gray-dark-v5 g-pa-0-20-20 text-justify">
-                        <p>The strategy we used to build the business was to attract higher valuations and focus on helping our portfolio companies gain growth, grow in a short space of time but keeping a sensible and safe attitude – this is achieved through the pursuit of a buy and build strategy. </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                
-                <!-- Card -->
-                <div class="card rounded-0 g-bg-gray-light-v3 g-brd-none">
-                  <div id="accordion-06-heading-04" class="u-accordion__header g-pa-20" role="tab">
-                    <h5 class="mb-0 text-uppercase g-font-size-default g-font-weight-700">
-                      <a class="collapsed d-flex justify-content-between g-color-primary g-text-underline--none--hover" href="#accordion-06-body-04" data-toggle="collapse" data-parent="#accordion-06" aria-expanded="false" aria-controls="accordion-06-body-04">
-                        Investment Experience
-                        <span class="u-accordion__control-icon g-ml-10">
-                          <i class="fa fa-angle-down"></i>
-                          <i class="fa fa-angle-up"></i>
-                        </span>
-                      </a>
-                    </h5>
-                  </div>
-                  <div id="accordion-06-body-04" class="collapse" role="tabpanel" aria-labelledby="accordion-06-heading-04" data-parent="#accordion-06">
-                    <div class="u-accordion__body g-color-gray-dark-v5 g-pa-0-20-20 text-justify">
-                      <p>Our team of investors from juniors to executive have a number of years of investment experience between them. Our aim is to ensure all our investors that this combined knowledge is applied in all investment, to assist our portfolio companies and individual clients to build therightportfolio and reach their maximum potential. </p>
-                    </div>
-                  </div>
-                </div>
-                <!-- End Card -->
-        </div>
+        <div class="col-sm-3"></div>
     </div>
     <div class="row g-mb-60">
-        <header class="text-center g-width-60x--md mx-auto g-mb-20">
-          <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
-            <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Equities <span class="g-color-primary">at a glance</span></h2>
-          </div>
-        </header>
-      <div class="col-sm-2"></div>
-      <div class="col-sm-4">
-          <img class="img-fluid float-right pl-5" src="img/equities4.jpg" alt="" >
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6">
+          <header class="text-center g-width-60x--md mx-auto g-mb-20">
+            <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+              <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">Equities <span class="g-color-primary">at a glance</span></h2>
+            </div>
+          </header>
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="img-fluid float-right pl-5" src="img/equities4.jpg" alt="" >
+            </div>
+            <div class="col-sm-8">
+              <ul>
+                <li>Trade in 16 Stock exchanges around the globe.</li>
+                <li>An extensive range of financial markets within your reach.</li>
+                <li>Stretch the risk across an extensive range of locations and markets.</li>
+                <li>In equities you are likely to get much better returns, more than you would when
+                 investing in cash deposits or bonds.</li>
+              </ul>
+            </div>
+        </div>
       </div>
-      <div class="col-sm-4">
-        <ul>
-          <li>Trade in 16 Stock exchanges around the globe.</li>
-          <li>An extensive range of financial markets within your reach.</li>
-          <li>Stretch the risk across an extensive range of locations and markets.</li>
-          <li>In equities you are likely to get much better returns, more than you would when
-           investing in cash deposits or bonds.</li>
-        </ul>
-      </div>
-      <div class="col-sm-2"></div>
+      <div class="col-sm-3"></div>
   </div>
   <div class="row g-mb-60">
-      <header class="text-center g-width-60x--md mx-auto g-mb-20">
-        <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
-          <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">What is <span class="g-color-primary">equity investment?</span></h2>
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6">
+          <header class="text-center g-width-60x--md mx-auto g-mb-20">
+            <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
+              <h2 class="h3 u-heading-v2__title g-color-gray-dark-v2 text-uppercase g-font-weight-600 text-uppercase">What is <span class="g-color-primary">equity investment?</span></h2>
+            </div>
+          </header>
+        <div class="row">
+          <div class="col-sm-4">
+            <img class="img-fluid float-right pl-5" src="img/equities6.png" alt="" >
+          </div>
+          <div class="col-sm-8">
+            <p class="text-justify ">Equity investment is when you buy one or more shares in a company and invest directly
+              in that business.</p>
+            <p class="text-justify ">You buy your equity shares and receive returns in the form of a dividend which the
+              company may choose to pay – often annually, however it can be at the company's
+              discretion. It's important to note that the value of your equity investment may increase
+              or fall depending on the underlying value of the company.Many investors, treat equity
+              trading as a large portion of their wealth management strategy and, if you choose to
+              increase your wealth across the world, equity trading via a wealth management firm is
+              one way of doing this.</p>
+          </div>
         </div>
-      </header>
-    <div class="col-sm-2"></div>
-    <div class="col-sm-4">
-        <img class="img-fluid float-right pl-5" src="img/equities6.png" alt="" >
+      </div>
+      <div class="col-sm-3"></div>
     </div>
-    <div class="col-sm-4">
-      <p class="text-justify ">Equity investment is when you buy one or more shares in a company and invest directly
-          in that business.</p>
-      <p class="text-justify ">You buy your equity shares and receive returns in the form of a dividend which the
-          company may choose to pay – often annually, however it can be at the company's
-          discretion. It's important to note that the value of your equity investment may increase
-          or fall depending on the underlying value of the company.Many investors, treat equity
-          trading as a large portion of their wealth management strategy and, if you choose to
-          increase your wealth across the world, equity trading via a wealth management firm is
-          one way of doing this.</p>
-    </div>
-    <div class="col-sm-2"></div>
-  </div>
   <div class="row g-mb-60">
       <header class="text-center g-width-60x--md mx-auto g-mb-20">
         <div class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
